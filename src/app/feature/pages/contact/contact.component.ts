6d4fff655cd7ebc6da9c6778630dbafa8d2359e3
AhmedMagdy9 <megoo9786@gmail.com>
2025-07-22 20:59:01 +0300

    contact edit

--- src/app/feature/pages/contact/contact.component.ts
+++ src/app/feature/pages/contact/contact.component.ts
@@ -19,17 +19,18 @@ export class ContactComponent {
     name: new FormControl('', [Validators.required,Validators.minLength(8), Validators.pattern(/^[\u0600-\u06FFa-zA-Z\s]+$/) ]),
     address: new FormControl('', [ Validators.required, Validators.minLength(10), Validators.pattern(/^[\u0600-\u06FFa-zA-Z\s]+$/) ]),
     phone: new FormControl('', [  Validators.required,  Validators.pattern(/^01[0125][0-9]{8}$/)  ]),
-    occasion: new FormControl(false) // checkbox
+    occasion: new FormControl(false) 
   });
 
   submit() {
-    if (this.userDetails.valid && this.orderService.order ) {
+    if (this.userDetails.valid && this.orderService.order.length !== 0 ) {
       let fullOrder = {"products":this.orderService.order,"customer":this.userDetails.value }
       this.sendOrder(fullOrder)
 
       
     } else {
       this.userDetails.markAllAsTouched();
+      this.alertService.error(' لا يوجد منتجات !')
     }
   }
 
